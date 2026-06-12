6dd1d699f6bcca7dbce295d5fc67347124216563
Mark-GCT <mark@gatewaycornerstonetech.com>
2026-06-11 23:45:59 -0500

    updated index, header, footer, less files, added svg

--- src/_data/client.js
+++ src/_data/client.js
@@ -14,7 +14,7 @@ module.exports = {
         state: "IL",
         zip: "62221",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/AjdJpTF2tKgm12iJ9",
+        mapLink: "https://www.google.com/maps/place/Kaesberg+Chiropractic+Clinic+PC/@38.5123722,-89.9777572,16z/data=!4m15!1m8!3m7!1s0x887602ec14c2930b:0xd373cdd193eaa488!2s100+Mascoutah+Ave,+Belleville,+IL+62220!3b1!8m2!3d38.5123722!4d-89.9777572!16s%2Fg%2F11crs9vg0b!3m5!1s0x887602ec3ffc59bd:0x2133a8168820f7a0!8m2!3d38.5123754!4d-89.9777929!16s%2Fg%2F1tgcsn0f?entry=ttu&g_ep=EgoyMDI2MDYwOS4wIKXMDSoASAFQAw%3D%3D",
     },
     address2: {
         lineOne: "3131 State St.",
@@ -23,7 +23,7 @@ module.exports = {
         state: "IL",
         zip: "62205",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/rxi6BsqqZEfwdeGE8",
+        mapLink: "https://www.google.com/maps/place/Kaesberg+Chiropractic+Clinic/@38.6130654,-90.1236139,17z/data=!4m15!1m8!3m7!1s0x87d8ad07d0faaaab:0x3b2e06827e7fd4ec!2s3131+State+St,+East+St+Louis,+IL+62205!3b1!8m2!3d38.6130654!4d-90.121039!16s%2Fg%2F11rg67q5k1!3m5!1s0x87d8ad07da03be61:0xce318600f56f7d4c!8m2!3d38.6130655!4d-90.121039!16s%2Fg%2F1vbl83j8?entry=ttu&g_ep=EgoyMDI2MDYwOS4wIKXMDSoASAFQAw%3D%3D",
     },
     socials: {
         facebook: "https://www.facebook.com/KaesbergChiropractic/",
